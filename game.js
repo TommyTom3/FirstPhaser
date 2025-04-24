6dd7f205e10079f10dcecb73109f306eb6ff33c9
TommyTom3 <thafner6316@gmail.com>
2025-04-23 22:30:24 -0400

    centered ball

--- game.js
+++ game.js
@@ -48,7 +48,7 @@ function create() {
 // Adding blue circle 
 const circleRadius = 30;
 const circleX = this.sys.game.canvas.width / 2;
-const circleY = this.sys.game.canvas.height * 2 / 3 + 50
+const circleY = this.sys.game.canvas.height * 2 / 3 + 50;
 
 //Create graphics object for circle 
 const circle = this.add.graphics();
