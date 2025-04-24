ea3c9ccf3ee19c2f894b5d7f7f92999690ab6ed4
TommyTom3 <thafner6316@gmail.com>
2025-04-23 22:24:34 -0400

    Added Draggable Blue Ball

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ function create() {
         this.sys.game.canvas.width,
         this.sys.game.canvas.height,
         0x00ff00
-    ).setOrigin(0.5);
+    ).setOrigin(0.5);   
 
     const background = this.add.image(
         this.cameras.main.centerX,
@@ -42,5 +42,42 @@ function create() {
 
     background.setDisplaySize(this.sys.game.canvas.width, this.sys.game.canvas.height);
 
-    console.log('Image size:', background.displayWidth, background.displayHeight);
+    console.log('Image size:', background.displayWidth, background.displayHeight);   
+
+
+// Adding blue circle 
+const circleRadius = 30;
+const circleX = this.sys.game.canvas.width / 3 - 50;
+const circleY = this.sys.game.canvas.height * 2 / 3 + 50
+
+//Create graphics object for circle 
+const circle = this.add.graphics();
+circle.fillStyle(0x0000ff, 1);
+circle.fillCircle(circleX, circleY, circleRadius);
+
+// Bring to front 
+circle.setDepth(10);
+
+//Make circle interactive
+circle.setInteractive(
+    new Phaser.Geom.Circle(circleX, circleY, circleRadius),
+    Phaser.Geom.Circle.Contains
+);
+
+// Enable dragging
+this.input.setDraggable(circle);
+circle.on('drag', (pointer, dragX, dragY) => {
+
+    const worldPoint = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
+    const adjustedX = worldPoint.x;
+    const adjustedY = worldPoint.y;
+    //this updates while dragging
+    circle.clear();
+    circle.fillStyle(0x0000ff, 1);
+    circle.fillCircle(adjustedX, adjustedY, circleRadius);
+
+    // Update interactive area to follow circle 
+    circle.input.hitArea.setTo(adjustedX, adjustedY, circleRadius);
+});
+
 }
